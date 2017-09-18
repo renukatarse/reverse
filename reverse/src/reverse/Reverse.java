cecdcd084302554abe20aa01a5d4e59fcfa97624
renuka <renutarse1@gmail.com>
2017-09-18 11:05:46 +0530

    reverse commit

---
+++ reverse/src/reverse/Reverse.java
@@ -0,0 +1,30 @@
+package reverse;
+
+
+	import java.io.*;
+	import java.util.*;
+
+
+	public class Reverse {
+
+
+		public static void main(String[] args) {
+	        String input="";
+	        System.out.println("Enter the input string");
+	        try
+	        {
+	            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	            input = br.readLine();
+	            char[] try1= input.toCharArray();
+	            for (int i=try1.length-1;i>=0;i--)
+	            System.out.print(try1[i]);
+	        }
+	        catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	}}
+
+
+
+
+
